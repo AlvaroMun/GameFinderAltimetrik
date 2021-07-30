c9a77cb70326b450901d1184cedc616f9cf17f32
Alvaro Muñoz <83349032+AlvaroMun@users.noreply.github.com>
2021-07-30 13:17:03 -0300

    add one column toggle functionality in main

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 document.addEventListener("DOMContentLoaded", function () {
-  let listOfCards = document.querySelector("#listContent");
   let mainContent = document.querySelector(".contentWrapper");
   let logOutBtn = document.querySelector("#logOutBtn");
   let searchBtn = document.querySelector("#searchBtn");
@@ -8,6 +7,8 @@ document.addEventListener("DOMContentLoaded", function () {
   let sideMenuToggle = document.querySelector(".sideMenuToggle");
   let pageTitle = document.querySelector("#pageTitle");
   let header = document.querySelector(".header");
+
+  let toggleListClicked = false;
 
   if (window.innerWidth <= 690) {
     let searchBtnMovil = document.querySelector("searchBtnMovil");
@@ -75,10 +76,6 @@ document.addEventListener("DOMContentLoaded", function () {
 
   /*--------end of events listeners---------- */
 
-  getGames()
-    .then((json) => ngFor(json.results))
-    .catch((err) => console.log(err));
-
   /*------Async functions------- */
   async function getGames() {
     mainContent.innerHTML = loader;
@@ -108,21 +105,46 @@ document.addEventListener("DOMContentLoaded", function () {
     return response;
   }
 
+  async function getDescription(id) {
+    const response = await fetch(
+      `https://api.rawg.io/api/games/${id}?key=795574157c904a59976d02fb5d132e4e`
+    );
+
+    if (response.ok) {
+      let data = await response.json();
+      let desc = await data.description;
+      return desc;
+    } else {
+      throw Error(response.statusText);
+    }
+  }
+
   /*------End of Async functions------- */
 
+  /*--------initial list game ---------*/
+  getGames()
+    .then((json) => ngFor(json.results))
+    .catch((err) => console.log(err));
+
+  let listItems = "";
   /*------- function for listing games------ */
   function ngFor(listOfGames) {
     let i = 1;
-    let listItems = "";
-    let li = "";
 
+    let li = "";
+    let gameDescription = null;
+
+    /*forEachGame-------------------------------------- */
     listOfGames.forEach((game) => {
       let genRes = game.genres;
       let genResString = "";
 
+      /*---get list genres------- */
       genRes.forEach((gen) => {
         genResString += gen.name + ", ";
       });
+
+      /*get desc for each game */
 
       li = `
       <li>
@@ -139,8 +161,8 @@ document.addEventListener("DOMContentLoaded", function () {
                             <div class="gameCardAtt">
                                 <h6>Release date</h6>
                                 <h6>${game.released}</h6>
-                                <h6>Genres</h6>
-                                <h6>${genResString}</h6>
+                                <h6 class="marg-l">Genres</h6>
+                                <h6 class="pad-l">${genResString}</h6>
                             </div>
                         </div>
                         <div class="gameCardIcons">
@@ -163,6 +185,7 @@ document.addEventListener("DOMContentLoaded", function () {
                             </a>
                         </div>
                     </div>
+                    <p class="cardDescriptionP">test</p>
                 </div>
             </button>
       </li>`;
@@ -172,23 +195,85 @@ document.addEventListener("DOMContentLoaded", function () {
       i++;
     });
 
-    ul = '<ul id="listContent">' + listItems + "</ul>";
+    /*end of FOREACH */
+
+    let ul = '<ul id="listContent" class="gridContent">' + listItems + "</ul>";
     mainContent.innerHTML = ul;
+
+    /*-------Event listener that depend of the list to be render first ------*/
 
     if (ul) {
       let btnCards = document.querySelectorAll(".btnCard");
+
+      /*------Cards Clicked--------- */
       btnCards.forEach((card) => {
         card.addEventListener("click", (e) => {
           console.log("dataset Att id", e.currentTarget.dataset.game);
         });
       });
+
+      /*let listContent = document
+          .querySelector("#listToggleBtn")
+          .addEventListener("click", toggleList);*/
+
+      /*Toggle between list of cards or grid of cards */
+
+      let gameCardDesc = document.querySelectorAll(".gameCardDesc");
+      let cardParagraph = document.querySelectorAll(".cardDescriptionP");
+      let gameCardAtt = document.querySelectorAll(".gameCardAtt");
+      let genResListed = document.querySelectorAll(".marg-l");
+      let genResValListed = document.querySelectorAll(".pad-l");
+
+      let listToggleBtn = document
+        .querySelector("#listToggleBtn")
+        .addEventListener("click", () => {
+          if (toggleListClicked) {
+            toggleListClicked = false;
+
+            listContent.classList.remove("cardListSelected");
+            gameCardDesc.forEach((desc) => {
+              desc.classList.remove("gameCardDescListSelected");
+            });
+            cardParagraph.forEach((descP) => {
+              descP.classList.remove("paragraphListSelected");
+            });
+            gameCardAtt.forEach((cardAtt) => {
+              cardAtt.classList.remove("gameCardAttListSelected");
+            });
+            genResListed.forEach((genRes) => {
+              genRes.classList.remove("marg-l-40");
+            });
+            genResValListed.forEach((genResVal) => {
+              genResVal.classList.remove("pad-l-40");
+            });
+          } else {
+            toggleListClicked = true;
+
+            listContent.classList.add("cardListSelected");
+            gameCardDesc.forEach((desc) => {
+              desc.classList.add("gameCardDescListSelected");
+            });
+            cardParagraph.forEach((descP) => {
+              descP.classList.add("paragraphListSelected");
+            });
+            gameCardAtt.forEach((cardAtt) => {
+              cardAtt.classList.add("gameCardAttListSelected");
+            });
+            genResListed.forEach((genRes) => {
+              genRes.classList.add("marg-l-40");
+            });
+            genResValListed.forEach((genResVal) => {
+              genResVal.classList.add("pad-l-40");
+            });
+          }
+        });
     }
   }
+
+  /*END OF NGFOR----------------------- */
   /*li.onclick = test();*/
 
-  function test() {
-    console.log("ahi va papaaaa");
-  }
+  function toggleList() {}
 
   function logOut() {
     window.location.replace("http://localhost:5500/GameFinderAltimetrik/");
