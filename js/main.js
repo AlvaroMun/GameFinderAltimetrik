78d5bd380befd1bdf24ebca09b59109a5f822461
Alvaro Muñoz <83349032+AlvaroMun@users.noreply.github.com>
2021-08-06 15:28:07 -0300

    restructured project, add snackbar to login

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 document.addEventListener("DOMContentLoaded", function () {
-
   let mainContent = document.querySelector(".contentWrapper");
   let logOutBtn = document.querySelector("#logOutBtn");
   let searchBtn = document.querySelector("#searchBtn");
@@ -22,7 +21,6 @@ document.addEventListener("DOMContentLoaded", function () {
     let searchBtnMovil = document.querySelector("searchBtnMovil");
   }
 
-
   /*let li = "";
   let listItems = "";*/
   let loader = `
@@ -32,7 +30,6 @@ document.addEventListener("DOMContentLoaded", function () {
 `;
 
   /*--------events listeners---------- */
-
 
   logOutBtn.addEventListener("click", logOut);
 
@@ -63,7 +60,6 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   });
 
-
   searchBtnMovil.addEventListener("click", () => {
     console.log(header.style.height);
     if (header.style.height === "170px") {
@@ -73,7 +69,6 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   });
 
-
   sideMenuToggle.addEventListener("click", () => {
     if (sideNavMenu.classList.contains("sideNav")) {
       sideNavMenu.style.left = "0px";
@@ -82,7 +77,6 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 
   document.addEventListener("click", (e) => {
-
     console.log(e.target);
     if (e.target.id !== "sideMenuToggle") {
       sideNavMenu.style.left = "-330px";
@@ -153,7 +147,6 @@ document.addEventListener("DOMContentLoaded", function () {
 
   /*--------end of events listeners---------- */
 
-
   /*------Async functions------- */
   async function getGames() {
     mainContent.innerHTML = loader;
@@ -182,7 +175,6 @@ document.addEventListener("DOMContentLoaded", function () {
 
     return response;
   }
-
 
   async function getDescription(id) {
     const response = await fetch(
@@ -589,11 +581,9 @@ document.addEventListener("DOMContentLoaded", function () {
   function closeModalAction() {
     modalWindow.style.display = "none";
     backgroundOutisdeModal.classList.remove("showBackgroundOutsideModal");
-
-
   }
 
   function logOut() {
-    window.location.replace("http://localhost:5500/GameFinderAltimetrik/");
+    window.location.replace("../login/index.html");
   }
 });
